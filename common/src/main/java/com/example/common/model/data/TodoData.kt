ef8738bdb930e284ad5350e324b45c0e4084f4d1
tatsuya nakajima <fuzuki.1228@gmail.com>
2022-07-12 20:02:11 +0900

    TodoDataをRoomのEntity化

--- common/src/main/java/com/example/common/model/data/TodoData.kt
+++ common/src/main/java/com/example/common/model/data/TodoData.kt
@@ -1,11 +1,16 @@
 package com.example.common.model.data
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
+import java.util.*
 
 @Parcelize
+@Entity(tableName = "todo_list_table")
 data class TodoData (
-    val id: Long,
+    @PrimaryKey(autoGenerate = true) val id: Long,
     val title: String,
-    val description: String
+    val description: String,
+    val limitDate: Date
 ): Parcelable
--- common/src/main/java/com/example/common/room/database/DataConverter.kt
+++ common/src/main/java/com/example/common/room/database/DataConverter.kt
@@ -1,4 +1,16 @@
 package com.example.common.room.database
 
+import androidx.room.TypeConverter
+import java.util.*
+
 class DataConverter {
+    @TypeConverter
+    fun fromTimeStamp(value: Long): Date {
+        return Date(value)
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date): Long {
+        return date.time
+    }
 }

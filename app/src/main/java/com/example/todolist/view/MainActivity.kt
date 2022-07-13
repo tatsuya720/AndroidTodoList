bfe8fb27136e89419b5b86e0e952dd070053d733
tatsuya nakajima <fuzuki.1228@gmail.com>
2022-07-13 19:24:50 +0900

    フォーカスが外れた際のキーボード非表示対応

--- app/src/main/java/com/example/todolist/view/MainActivity.kt
+++ app/src/main/java/com/example/todolist/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.todolist.view
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import com.example.todolist.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,5 +17,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.navHostFragment.setOnFocusChangeListener { view, hasFocus ->
+            if(hasFocus) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        }
+    }
+
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        binding.navHostFragment.requestFocus()
+        return super.dispatchTouchEvent(ev)
     }
 }

eaba7c89370dce170d6cb5c8ee3c19619e61ca72
tatsuya nakajima <fuzuki.1228@gmail.com>
2022-07-15 01:11:29 +0900

    いらないimport削除

--- common/src/main/java/com/example/common/model/repository/TodoDataRepositoryImpl.kt
+++ common/src/main/java/com/example/common/model/repository/TodoDataRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.example.common.model.repository
 import com.example.common.model.data.TodoData
 import com.example.common.room.dao.TodoDao
 import kotlinx.coroutines.flow.Flow
-import java.util.*
 import javax.inject.Inject
 
 class TodoDataRepositoryImpl @Inject constructor(

71731ae9694bee379affd84076ba6108aa6cd67d
tatsuya nakajima <fuzuki.1228@gmail.com>
2022-09-04 00:59:00 +0900

    難読化時のエラー対策

--- common/src/main/java/com/example/common/model/data/TodoData.kt
+++ common/src/main/java/com/example/common/model/data/TodoData.kt
@@ -1,11 +1,13 @@
 package com.example.common.model.data
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.util.*
 
+@Keep
 @Parcelize
 @Entity(tableName = "todo_list_table")
 data class TodoData (
